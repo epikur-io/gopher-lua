378dfe65be8c7dbb8f50aeec4d7321c7219465a4
Epikur <3p1kur@protonmail.ch>
2025-03-01 00:42:59 +0100

    added glua unit test for debug hooks

---
+++ _glua-tests/debug.lua
@@ -0,0 +1,27 @@
+
+local function test_debughooks()
+    local debug = require("debug")
+
+    local function test_call(a, b)
+        return a + b
+    end
+
+    local counter = 0
+
+    local function hook (why)
+        counter = counter + 1
+        -- print("hook reached: ", why)
+        -- print("function =", debug.getinfo(2, "n").name)
+    end -- hook
+
+    debug.sethook (hook, "r", 0)
+
+    for i = 1, 10 do
+        test_call(i, i-1)
+    end -- for
+
+    -- expect debug hook to be called 10 times
+    assert(counter == 10)
+end
+
+test_debughooks()
