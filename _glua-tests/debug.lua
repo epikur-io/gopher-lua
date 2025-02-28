ff28a10c99cc1255dec66d02a9bbcf7228e95fb8
Epikur <3p1kur@protonmail.ch>
2025-03-01 00:56:04 +0100

    code formatting

--- _glua-tests/debug.lua
+++ _glua-tests/debug.lua
@@ -8,13 +8,14 @@ local function test_debughooks()
 
     local counter = 0
 
-    local function hook (why)
+    local function hook(why)
         counter = counter + 1
         -- print("hook reached: ", why)
         -- print("function =", debug.getinfo(2, "n").name)
     end -- hook
 
-    debug.sethook (hook, "r", 0)
+    debug.sethook(hook, "r", 0)
+    debug.sethook()
 
     for i = 1, 10 do
         test_call(i, i-1)
